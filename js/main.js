b4f1c04622e95f6dfe06cd8c507c9c92ad801cab
Maksim2103 <47346663+Maksim2103@users.noreply.github.com>
2021-02-26 16:35:01 +0300

    add app

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(function () {
+  $('.top__slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    fade: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          dots: false,
+        },
+      },
+    ],
+  });
+  $('.reviews__slider').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1141,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 846,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 586,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+});
